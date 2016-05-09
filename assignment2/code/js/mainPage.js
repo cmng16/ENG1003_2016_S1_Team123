a66b7ae57603cece57a421bf56b2148366b38459
JGreen3491 <jdgre4@student.monash.edu>
2016-05-09 13:11:49 +1000

    Update mainPage.js

--- assignment2/code/js/mainPage.js
+++ assignment2/code/js/mainPage.js
@@ -1,7 +1,10 @@
 // Code for the main app page (locations list).
-
+  
 // This is sample code to demonstrate navigation.
 // You need not use it for final app.
+//We can push a location which is added through the 'add location' page into this array.
+//This also needs to be saved in localStorage
+var savedLocations = []
 
 function viewLocation(locationName)
 {
