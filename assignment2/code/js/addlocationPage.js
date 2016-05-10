5f792253d09fc621e1da8a18e4d088ee2e6b7e14
cmng16 <cmng16@student.monash.edu>
2016-05-10 15:44:09 +1000

    changed the button command and linked it up to a function
    on click now links up and runs the function addLocation () which is not
    complete yet

--- assignment2/code/js/addlocationPage.js
+++ assignment2/code/js/addlocationPage.js
@@ -1,6 +1,11 @@
 // Code for the Add Location page.
 
-
+function addLocation ()
+{
+    var locationRef = document.getElementById("location")
+    var nicknameRef = document.getElementById("nickname")
+    
+}
  
 // Give maps API time to load, then initialise map.
 setTimeout(initMap, 1000);
