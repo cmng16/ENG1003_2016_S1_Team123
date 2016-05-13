86b6d33f60721d92043da183f4196cc705d81521
cmng16 <cmng16@student.monash.edu>
2016-05-13 13:54:37 +1000

    major changes to the following files
    these changes include the addition of necessary formats and layouts as
    well and incomplete functions which are required to be completed to
    carry out the functions of adding current locations and removal of saved
    location

--- assignment2/code/js/addlocationPage.js
+++ assignment2/code/js/addlocationPage.js
@@ -1,11 +1,78 @@
 // Code for the Add Location page.
 
-function addLocation ()
+function addLocation()
 {
-    var locationRef = document.getElementById("location")
+    var address = document.getElementById("location")
     var nicknameRef = document.getElementById("nickname")
-    
+     geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location;
+            });
+        } 
+     else 
+     {
+        alert("Geocode was not successful for the following reason: " + status);
+        }
+    });
 }
+
+//Adds current location to the page
+function addCurrentLocation()
+{
+    if (navigator.geolocation)
+    {     
+        positionOptions = {
+            enableHighAccuracy: true,
+            timeout: Infinity, 
+            maximumAge: 0
+        }; 
+
+        displayElementsWithClass("gpsError", false);
+        navigator.geolocation.watchPosition(showCurrentLocation, errorHandler, positionOptions);
+    }
+    else
+    {
+        displayElementsWithClass("gpsValue", false);
+    }
+
+    function errorHandler(error)
+    {
+        if (error.code == 1)
+        {
+           alert("Location access denied by user.");
+        }
+        else if (error.code == 2)
+        {
+           alert("Location unavailable.");
+        }
+        else if (error.code == 3)
+        {
+           alert("Location access timed out");
+        }
+        else
+        {
+           alert("Unknown error getting location.");
+        }
+    }
+
+    function showCurrentLocation(position)
+    {
+        // Demonstrate the current latitude and longitude:
+        document.getElementById("latValue").innerHTML = Number(position.coords.latitude).toFixed(4);
+        document.getElementById("longValue").innerHTML = Number(position.coords.longitude).toFixed(4);
+        document.getElementById("accuracyValue").innerHTML = Number(position.coords.accuracy).toFixed(2);  
+    }
+  
+};
  
 // Give maps API time to load, then initialise map.
 setTimeout(initMap, 1000);
+// code used to delete the location
+
+function removeLocation()
+{
+    
+};
